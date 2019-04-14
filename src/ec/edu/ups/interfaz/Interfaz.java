f3314bf9a03e9db8ccb811072636e1c07dc50624
Darwin <Darwin@DESKTOP-5O0JVM5>
2019-04-14 01:02:17 -0500

    Creacion de métodos abstractos en la clase Interfaz

--- src/ec/edu/ups/interfaz/Interfaz.java
+++ src/ec/edu/ups/interfaz/Interfaz.java
@@ -5,9 +5,20 @@
  */
 package ec.edu.ups.interfaz;
 
-
+/**
+ * Clase Interfaz donde contiene los metodos de las clases hijas, el cual todos
+ * realizan la misma accion pero de manera diferente.
+ *
+ * @author Darwin Leon, Est.
+ */
 public interface Interfaz {
 
+    public abstract void transportar();
 
+    public abstract void arrancar();
+
+    public abstract void frenar();
+
+    public abstract void estacionar();
 
 }
