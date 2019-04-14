cd31a2ec8d93b901076fd36e279358f5a1a303cf
Darwin <Darwin@DESKTOP-5O0JVM5>
2019-04-13 20:45:08 -0500

    Creacion del proyecto con las clases respectivas

---
+++ src/ec/edu/ups/clases/Transporte.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.ups.clases;
+
+/**
+ * Clase "abuelo" donde van la mayoria de atributos que serán heredados Metodos
+ * setters y getters
+ *
+ * @author Darwin Leon, Est.
+ */
+public class Transporte {
+
+    
+
+}
